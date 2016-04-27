f411bd88f8fe4ef2b8aef1fac91ae0a5b8f356cf
christopher britz <christopher.britz@capitalone.com>
2016-04-26 21:52:44 -0400

    added twitter oauth support with passportjs

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express"),
   AlexaSkills = require("alexa-skills"),
   Tweetbot = require("./twitter/api/tweetbot"),
   tweetBot = new Tweetbot(),
+  passport = require('passport'),
+  Strategy = require('passport-twitter').Strategy,
   app = express(),
   port = process.env.PORT || 8081,
   alexa = new AlexaSkills({
@@ -10,11 +12,66 @@ var express = require("express"),
     applicationId: process.env.ALEXA_APP_ID || "HelloWorld"
   });
 
-console.log("Started listening on", port)
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 
-app.use("/auth/success", function(resp){
-  console.log("SUCCESS::", resp);
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.use(new Strategy({
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+  },
+  function(token, tokenSecret, profile, cb) {
+    // In this example, the user's Twitter profile is supplied as the user
+    // record.  In a production-quality application, the Twitter profile should
+    // be associated with a user record in the application's database, which
+    // allows for account linking and authentication with other identity
+    // providers.
+    return cb(null, profile);
+  }));
+
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  In a
+// production-quality application, this would typically be as simple as
+// supplying the user ID when serializing, and querying the user record by ID
+// from the database when deserializing.  However, due to the fact that this
+// example does not have a database, the complete Twitter profile is serialized
+// and deserialized.
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
 });
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+app.get('/login/twitter',
+  passport.authenticate('twitter'));
+
+app.get('/login/twitter/return',
+  passport.authenticate('twitter', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('profile', { user: req.user });
+  });
+
+console.log("Started listening on", port)
 
 app.get("/search", function(req, res){
   tweetBot.getSearch({
